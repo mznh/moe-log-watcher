4ed404d3e9a573a3975670cf05c86b6c5586b7f6
mznh <hiromtym@gmail.com>
2022-01-10 08:21:37 +0900

    複数行にまたがるメッセージに対応

--- main.rb
+++ main.rb
@@ -1,24 +1,21 @@
 #!/usr/bin/env ruby
 
 require 'pp'
+require 'json'
 require 'listen'
 require 'digest/md5'
 require 'time'
 
-# log/ログ : mlog_***_**_**.txt の1行
-# msg/メッセージ : logを解釈してオブジェクトにしたもの
-
-#TODO オンメモリで管理するログはredisに移したい
 
 class MoEMessage
+
   attr_accessor :date,:time,:message, :finger_print
   attr_accessor :received_date, :received_time
-  def initialize raw_log, log_pattern
-    res = raw_log.match(log_pattern)
-    
-    @date = res[:date]
-    @time = res[:time]
-    @message = res[:message]
+
+  def initialize date,time,message
+    @date = date
+    @time = time
+    @message = message
     # 比較用にハッシュ値を格納しておく
     @finger_print = Digest::MD5.hexdigest(@date+@time+@message) 
     @received_date = Time.now.strftime("%y/%m/%d")
@@ -27,10 +24,26 @@ class MoEMessage
   def print
     puts inspect
   end
+
   def inspect
     "#{@date} #{@time}: #{@message}"
   end
+
+  def to_json
+    { date: @date, time: @time, message: @message,
+      finger_print: @finger_print,  
+      received_date: @received_date, 
+      received_time: @received_time,
+    }.to_json
+  end
+  def self.from_json json_object
+    msg = MoEMessage.new(json_object["date"],json_object["time"],json_object["message"])
+    msg.received_date = json_object["received_date"]
+    msg.received_time = json_object["received_time"]
+    msg 
+  end
 end
+
 
 class MoEMessageLogger
   # 汎用 
@@ -57,15 +70,22 @@ class MoEMessageLogger
     puts "=========================================="
 
     # 過去の処理済みlogをオンメモリに読み込み
-    @msg_store = File.readlines(outer_log_path).map do |log_line| 
-      msg = MoEMessage.new(log_line,LOG_PATTERN)
+    @msg_store = File.readlines(outer_log_path).map do |json_line| 
+      msg = MoEMessage.from_json(JSON.parse(json_line))
       #puts "処理済みコメント： #{log_line}"
       msg
     end
 
     # 処理済みlog書き込み用FileStream
     @log_store_fs = File.open(outer_log_path,"a")
+  end
 
+  # 特定のログファイルを全て読み込んでMoEMessage オブジェクトの配列に変換
+  def read_log_file(file_path)
+    body = File.read(file_path, encoding: 'Shift_JIS:UTF-8')
+    res =  body.split(/(?<date>\d{2}\/\d{2}\/\d{2}) (?<time>\d{2}:\d{2}:\d{2}):/)
+    res.shift # 先頭の空文字列を削除
+    res.map(&:chomp).each_slice(3).map{|date,time,message| MoEMessage.new(date,time,message) }
   end
 
   def run
@@ -73,21 +93,13 @@ class MoEMessageLogger
     listener = Listen.to(@userdata_path) do |modified, added, removed| 
       # メッセージログだけ抜き出す
       modified_message_log_list = modified.grep(/mlog/)
-      if modified_message_log_list.empty? then
-        # ログ以外が改変されてたら無視
-        next
-      end
-
-      #MoE側の設定で保存するmlogは一つだけと仮定
       modified_message_log_list.each do |modified_message_log|
         # 改変があったログファイルを全部読み込み
-        modified_logs = File.readlines(modified_message_log, encoding: Encoding::Shift_JIS).map{|raw_log| raw_log.chomp.encode(Encoding::UTF_8)}
-        modified_logs.each do |log|
-          msg = MoEMessage.new(log,LOG_PATTERN)
+        read_log_file(modified_message_log).each do |msg|
           if not @msg_store.any?{|processed_log| processed_log.finger_print == msg.finger_print} then
             #新しいメッセージならば
-            #処理済み
-            @log_store_fs.write log + "\n"
+            @msg_store << msg
+            @log_store_fs.write(msg.to_json + "\n") 
             throw_message msg
           end
         end
@@ -105,7 +117,6 @@ class MoEMessageLogger
   end
 
   def throw_message msg
-    @msg_store << msg
     puts "#{msg.received_date} #{msg.received_time}: 受理"
     msg.print
   end
